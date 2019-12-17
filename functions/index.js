6cdaca69fade64cb3887cb8520935a5458019bd9
Pallav Jain <pallavjain409@gmail.com>
2019-12-17 14:06:09 +0530

    route to update profile

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,7 @@ const express = require("express");
 const app = express();
 
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login, uploadImage } = require("./handlers/users");
+const { signup, login, uploadImage, addUserDetails } = require("./handlers/users");
 const FBAuth = require("./util/fbAuth");
 
 
@@ -17,7 +17,7 @@ app.post("/screams", FBAuth, postOneScream);
 app.post("/signup", signup);
 app.post("/login", login);
 app.post("/user/image", FBAuth,uploadImage);
-
+app.post("/user", FBAuth, addUserDetails)
 // eslint-disable-next-line consistent-return
 
 exports.api = functions.https.onRequest(app);
--- functions/util/validators.js
+++ functions/util/validators.js
@@ -46,3 +46,17 @@ exports.validateLogin = user => {
     valid: Object.keys(error).length === 0 ? true : false
   };
 };
+
+exports.userReducer = (data)=>{
+  let userDetails = {}
+  if (!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
+  if (!isEmpty(data.location.trim())) userDetails.location = data.location;
+  if (!isEmpty(data.website.trim()))
+  {
+    if (data.website.trim().substring(0, 4)!== "http"){
+        userDetails.website = `http://${data.website}`
+    }
+    else userDetails.website = data.website;
+  }
+  return userDetails
+}
