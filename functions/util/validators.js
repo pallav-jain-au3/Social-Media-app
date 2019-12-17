5351f1f79c540394a21a34ebdd33748dd7374d3f
Pallav Jain <pallavjain409@gmail.com>
2019-12-17 11:36:53 +0530

    Test for Signup

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -1,6 +1,5 @@
 const isEmpty = string => string.trim() === "";
 const isValidEmail = email => {
-  console.log(email);
   const emailRegEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (email.match(emailRegEx)) {
     return true;
--- functions/util/validators.spec.js
+++ functions/util/validators.spec.js
@@ -1,58 +1,171 @@
-const { validateLogin } = require("./validators");
+const { validateLogin, validateSignup } = require("./validators");
 
 describe("Validator tests", () => {
-
-    describe("validateLogin tests", () => {
-
-        test("returns valid true and zero errors if email and password are not empty", () => {
-            const user = {
-                email: "user@user.com",
-                password: "password"
-            };
-            const response = validateLogin(user)
-            expect(response.valid).toBeTruthy();
-            expect(Object.keys(response.error)).toHaveLength(0);
-        })
-
-        test("returns valid false and error when email is empty", () => {
-            const user = {
-                email: "",
-                password: "password"
-            };
-            const response = validateLogin(user);
-            expect(response.valid).toBeFalsy();
-            expect(Object.keys(response.error)).toHaveLength(1);
-            expect(response.error.email).toBe("Must not be empty.");
-        })
-
-        test("returns valid false and error when password is empty", () => {
-            const user = {
-                email: "user@user.com",
-                password: ""
-            };
-            const response = validateLogin(user);
-            expect(response.valid).toBeFalsy();
-            expect(Object.keys(response.error)).toHaveLength(1);
-            expect(response.error.password).toBe("Must not be empty");
-        })
-
+  describe("validateLogin tests", () => {
+    test("returns valid true and zero errors if email and password are not empty", () => {
+      const user = {
+        email: "user@user.com",
+        password: "password"
+      };
+      const response = validateLogin(user);
+      expect(response.valid).toBeTruthy();
+      expect(Object.keys(response.error)).toHaveLength(0);
     });
 
-    describe("validateSignup tests", () => {
-
-        test("valid is true if signup details are valid", () => { });
-
-        test("valid is false if email is empty", () => { });
-
-        test("valid is false if email is invalid", () => { });
-
-        test("valid is false if password is empty", () => { });
-
-        test("valid is false if confirm password is empty", () => { });
-
-        test("valid is false if confirm password and password don't match", () => { });
-
-        test("valid is false if handle is empty", () => { });
+    test("returns valid false and error when email is empty", () => {
+      const user = {
+        email: "",
+        password: "password"
+      };
+      const response = validateLogin(user);
+      expect(response.valid).toBeFalsy();
+      expect(Object.keys(response.error)).toHaveLength(1);
+      expect(response.error.email).toBe("Must not be empty.");
     });
 
-})
+    test("returns valid false and error when password is empty", () => {
+      const user = {
+        email: "user@user.com",
+        password: ""
+      };
+      const response = validateLogin(user);
+      expect(response.valid).toBeFalsy();
+      expect(Object.keys(response.error)).toHaveLength(1);
+      expect(response.error.password).toBe("Must not be empty");
+    });
+  });
+
+  describe("validateSignup tests", () => {
+    test("valid is true if signup details are valid", () => {
+      let user = {
+        email: "p@gmail.com",
+        password: "12345678",
+        confirmPassword: "12345678",
+        handle: "12345678"
+      };
+
+      const response = validateSignup(user);
+      const expected = {
+        error: {},
+        valid: true
+      };
+      expect(response.valid).toBeTruthy();
+      expect(Object.keys(response.error).length).toBe(0);
+      expect(response).toMatchObject(expected);
+    });
+
+    test("valid is false if email is empty", () => {
+      let user = {
+        email: "",
+        password: "12345678",
+        confirmPassword: "12345678",
+        handle: "12345678"
+      };
+
+      const response = validateSignup(user);
+      const expected = {
+        error: { email: "Must not be empty" },
+        valid: false
+      };
+      expect(response.valid).toBeFalsy();
+      expect(response.error.email).toBe("Must not be empty");
+      expect(Object.keys(response.error).length).toBe(1);
+      expect(response).toMatchObject(expected);
+    });
+
+    test("valid is false if email is invalid", () => {
+      let user = {
+        email: "pallav",
+        password: "12345678",
+        confirmPassword: "12345678",
+        handle: "12345678"
+      };
+
+      const response = validateSignup(user);
+      const expected = {
+        error: { email: "Email must be a valid string" },
+        valid: false
+      };
+      expect(response.valid).toBeFalsy();
+      expect(response.error.email).toBe("Email must be a valid string");
+      expect(Object.keys(response.error).length).toBe(1);
+      expect(response).toMatchObject(expected);
+    });
+
+    test("valid is false if password is empty", () => {
+      let user = {
+        email: "pallav@gmail.com",
+        password: "",
+        confirmPassword: "",
+        handle: "12345678"
+      };
+
+      const response = validateSignup(user);
+      const expected = {
+        error: { password: "Must not be empty" },
+        valid: false
+      };
+      expect(response.valid).toBeFalsy();
+      expect(response.error.password).toBe("Must not be empty");
+      expect(Object.keys(response.error).length).toBe(1);
+      expect(response).toMatchObject(expected);
+    });
+
+    test("valid is false if confirm password is empty", () => {
+      let user = {
+        email: "pallav@gmail.com",
+        password: "12345678",
+        confirmPassword: "",
+        handle: "12345678"
+      };
+
+      const response = validateSignup(user);
+      const expected = {
+        error: { confirmPassword: "Password must match" },
+        valid: false
+      };
+      expect(response.valid).toBeFalsy();
+      expect(response.error.confirmPassword).toBe("Password must match");
+      expect(Object.keys(response.error).length).toBe(1);
+      expect(response).toMatchObject(expected);
+    });
+
+    test("valid is false if confirm password and password don't match", () => {
+      let user = {
+        email: "pallav@gmail.com",
+        password: "12345678",
+        confirmPassword: "11111",
+        handle: "12345678"
+      };
+
+      const response = validateSignup(user);
+      const expected = {
+        error: { confirmPassword: "Password must match" },
+        valid: false
+      };
+      expect(response.valid).toBeFalsy();
+      expect(response.error.confirmPassword).toBe("Password must match");
+      expect(Object.keys(response.error).length).toBe(1);
+      expect(response).toMatchObject(expected);
+    });
+
+    test("valid is false if handle is empty", () => {
+      let user = {
+        email: "p@gmail.com",
+        password: "12345678",
+        confirmPassword: "12345678",
+        handle: ""
+      };
+
+      const response = validateSignup(user);
+      const expected = {
+        error: { handle: "Must not be empty" },
+        valid: false
+      };
+      expect(response.valid).toBeFalsy();
+      expect(response.error.handle).toBe("Must not be empty");
+      expect(Object.keys(response.error).length).toBe(1);
+      expect(response).toMatchObject(expected);
+    });
+  });
+});
