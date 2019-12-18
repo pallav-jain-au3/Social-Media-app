a69b6152be226e7d95cfab95d17fae1cf383abd8
Pallav Jain <pallavjain409@gmail.com>
2019-12-18 12:03:47 +0530

    post commit

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -39,3 +39,34 @@ exports.postOneScream = (req, res) => {
       console.log(err);
     });
 };
+
+exports.commentOnScream = (req, res) => {
+  console.log(req.body);
+  if (req.body.body.trim() == "") {
+    return res.status(400).json({ error: "Must not be empty" });
+  }
+  let newComment = {
+    body: req.body.body,
+    screamId: req.params.screamId,
+    userHandle: req.user.handle,
+    imageUrl: req.user.imageUrl,
+    createdAt: new Date().toISOString()
+  };
+
+  db.doc(`/screams/${req.params.screamId}`)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "Not found" });
+      }
+
+      return db.collection("comments").add(newComment);
+    })
+    .then(() => {
+      res.status(201).json({ message: "comment added successfully" });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err.code });
+    });
+};
--- functions/index.js
+++ functions/index.js
@@ -4,20 +4,22 @@ const functions = require("firebase-functions");
 const express = require("express");
 const app = express();
 
-const { getAllScreams, postOneScream } = require("./handlers/screams");
+const {
+  getAllScreams,
+  postOneScream,
+  commentOnScream
+} = require("./handlers/screams");
 const { signup, login, uploadImage } = require("./handlers/users");
 const FBAuth = require("./util/fbAuth");
-
 
 //Scream Routes
 app.get("/screams", getAllScreams);
 app.post("/screams", FBAuth, postOneScream);
+app.post("/screams/:screamId/comment", FBAuth, commentOnScream);
 
 // user routes
 app.post("/signup", signup);
 app.post("/login", login);
-app.post("/user/image", FBAuth,uploadImage);
-
-// eslint-disable-next-line consistent-return
+app.post("/user/image", FBAuth, uploadImage);
 
 exports.api = functions.https.onRequest(app);
